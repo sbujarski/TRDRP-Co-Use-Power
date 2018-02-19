3ac42f3eb04bea4c3473d1efabf0991e9f9061e8
sbujarski <s.bujarski@gmail.com>
2018-02-19 14:45:19 -0800

    Adding R syntax

---
+++ TRDRP-Co-Use-Power.R
@@ -0,0 +1,13 @@
+#TRDRP Co-Use Grant Application
+#PI: Spencer Bujarski
+#Power Calculation via simulation
+
+library(SpPack)
+library(multilevel)
+library(dplyr)
+library(plyr)
+library(tidyr)
+library(ggplot2)
+library(gmodels) #CrossTable Function
+library(Hmisc) #rcorr
+
